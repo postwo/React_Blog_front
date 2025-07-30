6c13b2bf66b13265031ccc376b84a212a4d865b8
postwo <codo7717@naver.com>
2025-07-30 22:33:21 +0900

    더미 데이터 추가

---
+++ board-front/src/mocks/latest-board-list.mock.ts
@@ -0,0 +1,57 @@
+import { BoardListItem } from '../types/interface';
+
+// 더미(Dummy) 데이터
+const latestBoardListMock: BoardListItem[] = [
+  {
+    boardNumber: 1,
+    title: '오늘 점심 뭐먹지',
+    content: '오늘 점심 맛있어',
+    boardTitleImage:
+      'https://previews.123rf.com/images/jimmyan8511/jimmyan85111508/jimmyan8511150800008/43125907-%EB%B0%94%EB%8B%B7%EA%B0%80.jpg',
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: '2023.08.18. 00:54:27',
+    writerNickname: '안녕하세요 ',
+    writerProfileImage:
+      'https://previews.123rf.com/images/jimmyan8511/jimmyan85111508/jimmyan8511150800008/43125907-%EB%B0%94%EB%8B%B7%EA%B0%80.jpg',
+  },
+  {
+    boardNumber: 1,
+    title: '오늘 점심 뭐먹지',
+    content: '오늘 점심 맛있어',
+    boardTitleImage: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: '2023.08.18. 00:54:27',
+    writerNickname: '안녕하세요 ',
+    writerProfileImage: null,
+  },
+  {
+    boardNumber: 1,
+    title: '오늘 점심 뭐먹지',
+    content: '오늘 점심 맛있어',
+    boardTitleImage: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: '2023.08.18. 00:54:27',
+    writerNickname: '안녕하세요 ',
+    writerProfileImage: null,
+  },
+  {
+    boardNumber: 1,
+    title: '오늘 점심 뭐먹지',
+    content: '오늘 점심 맛있어',
+    boardTitleImage: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: '2023.08.18. 00:54:27',
+    writerNickname: '안녕하세요 ',
+    writerProfileImage: null,
+  },
+];
+
+export default latestBoardListMock;
