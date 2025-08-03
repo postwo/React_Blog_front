f6fa5d9bfc733d7f523c71ce7fa44cafde436cb6
postwo <codo7717@naver.com>
2025-08-03 13:15:12 +0900

    회원가입 dto 구현

--- board-front/src/apis/response/index.ts
+++ board-front/src/apis/response/index.ts
@@ -1,4 +1,4 @@
-import ResponsDto from './Response.dto';
+import ResponsDto from './rsponse.dto';
 
 //index.ts가 없으면 import는 가능하지만, "경로가 길어지고, 관리도 복잡해지며, 협업 시 혼선이 생기기 쉬워진다
 //그러므로 index 를 통해서 하나로 모아서 내보내는거다
