4bd48146131dfa22d7233ad3b56dd793980d4150
postwo <codo7717@naver.com>
2025-08-10 00:28:01 +0900

    게시물 상세보기 상단 스타일

--- board-front/src/views/Board/Detail/index.tsx
+++ board-front/src/views/Board/Detail/index.tsx
@@ -7,10 +7,20 @@ import { commentListMock } from 'mocks';
 import CommentItem from 'components/CommentItem';
 import Pagenation from 'components/PageNation';
 
+import DefaultProfileImage from 'assets/image/default-profile-image.png';
+
 //            component: 게시물 상세 화면 컴포넌트      //
 export default function BoardDetail() {
   //        component: 게시물 상세 상단 컴포넌트      //
   const BoardDetailTop = () => {
+    //        state: more 버튼 상태        //
+    const [showMore, setShowMore] = useState<boolean>(false);
+
+    //        event handler: more 버튼 클릭 이벤트 처리   //
+    const onMoreButtonClickHandler = () => {
+      setShowMore(!showMore);
+    };
+
     //        render: 게시물 상세 상단 컴포넌트 렌더링   //
     return (
       <div id="board-detail-top">
@@ -18,19 +28,25 @@ export default function BoardDetail() {
           <div className="board-detail-title">{'오늘 점심 뭐 먹을까'}</div>
           <div className="board-detail-top-sub-box">
             <div className="board-detail-write-info-box">
-              <div className="board-detail-writer-profile-image"></div>
+              <div
+                className="board-detail-writer-profile-image"
+                style={{ backgroundImage: `url(${DefaultProfileImage})` }}
+              ></div>
               <div className="board-detail-writer-nickname">{'타코'}</div>
               <div className="board-detail-info-divider">{'|'}</div>
               <div className="board-detail-write-date">{'2025.05.12'}</div>
             </div>
-            <div className="icon-button">
+            <div className="icon-button" onClick={onMoreButtonClickHandler}>
               <div className="icon more-icon"></div>
             </div>
-            <div className="board-detail-more-box">
-              <div className="board-detail-update-button">{'수정'}</div>
-              <div className="divider"></div>
-              <div className="board-detail-delete-button">{'삭제'}</div>
-            </div>
+            {/*showmore 가 true이면 수정 삭제를 보여준다 */}
+            {showMore && (
+              <div className="board-detail-more-box">
+                <div className="board-detail-update-button">{'수정'}</div>
+                <div className="divider"></div>
+                <div className="board-detail-delete-button">{'삭제'}</div>
+              </div>
+            )}
           </div>
         </div>
         <div className="divider"></div>
@@ -38,7 +54,10 @@ export default function BoardDetail() {
           <div className="board-detail-main-text">
             {'dhsmasfdjlasjflkjasljfl'}
           </div>
-          <div className="board-detail-main-image"></div>
+          <img
+            className="board-detail-main-image"
+            src="https://searchad-phinf.pstatic.net/MjAyNTA1MTJfMTE3/MDAxNzQ3MDU4MDAxNTI3.1lWFa4ZrkRElAsXOFNTCU75puedC9rQWpzgpiWhkaccg.YEFW2kaMcjUdVr0U9rZfs5xtxvzfmxWJkrku2org9ZUg.JPEG/880451-ab7a260d-f7e5-4f36-899f-91996138000b.jpg&w=54&h=54&rs=2&qlt=100"
+          />
         </div>
       </div>
     );
